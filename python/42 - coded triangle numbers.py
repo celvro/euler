79d834117b362fab5b5dd20427f9f3f6f754668f
celvro <dwlfk2@mst.edu>
2014-06-08 17:07:55 -0500

    updated function names

--- python/42 - coded triangle numbers.py
+++ python/42 - coded triangle numbers.py
@@ -1,4 +1,4 @@
-from util import is_triangle_number
+from util import is_triangle
 from util import word_value
 
 for line in open("42 - words.txt"):
@@ -7,7 +7,7 @@ for line in open("42 - words.txt"):
     
 x = 0    
 for word in words:
-    if(is_triangle_number(word_value(word))):
+    if is_triangle(word_value(word)):
         x+=1
     
 print x
